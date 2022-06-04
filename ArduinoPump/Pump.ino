00ff696b74dc75d6b6a962cacad59f24ff612f8c
matzesoft <48252507+matzesoft@users.noreply.github.com>
2022-06-04 18:45:30 +0200

    Check pump active with var

--- ArduinoPump/Pump.ino
+++ ArduinoPump/Pump.ino
@@ -1,22 +1,26 @@
 #define PUMP_PIN 4
 
+bool pumpEnabled = false;
+
 void setupPump(void) {
   pinMode(PUMP_PIN, OUTPUT);
-  digitalWrite(PUMP_PIN, LOW);
+  disablePump();
 }
 
 void activatePump(void) {
   Serial.println("Pump will be enabled.");
   digitalWrite(PUMP_PIN, HIGH);
+  pumpEnabled = true;
   setPumpActive(true);
 }
 
 void disablePump(void) {
   Serial.println("Pump will be disabled.");
   digitalWrite(PUMP_PIN, LOW);
+  pumpEnabled = false;
   setPumpActive(false);
 }
 
-bool pumpIsActive(void) {
-  return (digitalRead(PUMP_PIN) == HIGH);
+bool pumpIsEnabled(void) {
+  return pumpEnabled;
 }
